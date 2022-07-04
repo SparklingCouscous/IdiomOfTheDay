7baf698ef25fb382f502173fbc68fa9434e5675c
Sean Groenewald <seang@bbd.co.za>
2022-07-04 20:43:35 +0200

    Added Idiom table model definition

---
+++ backend/database/models.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+
+const ModelNames = {
+  Idiom: 'Idiom',
+};
+
+const Models = {
+  Idiom: {
+    IdiomId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Idiom: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Meaning: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+}
+
+module.exports = {
+  Models,
+  ModelNames,
+};
