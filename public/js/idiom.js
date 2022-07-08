2698cdb679b17c41b159d0e407ba5024ba1c7451
RushilDaya-BBD <rushild@bbd.co.za>
2022-07-08 16:29:56 +0200

    clean up

--- public/js/idiom.js
+++ public/js/idiom.js
@@ -1,35 +1,12 @@
 
-
-
-
-var Idioms = [
-    {idiom: "Man of Few Words", description: "A person who does not speak a great deal; someone who talks with as few words as possible.", rating: 1},
-    {idiom: "Dropping Like Flies", description: "To fall down ill or to die in large numbers.", rating: 1},
-    {idiom: "Short End of the Stick", description: "Getting the bad end of a deal, or receiving the least desirable outcome from something.", rating: 1},
-];
-
-
-let idiomCounter = 0;
-
-let reply = [];
 function initialIdiom()
 {
-    // fetch('http://localhost:8080/api/idiom') 
-    // .then(response => { 
-    //     const data = response.json();
-    //     console.log(data);
-    //     console.log(data.then)
-    //     // console.log(data[["PromiseResult"]]);
-    // });
-    
    
     fetch('http://localhost:8080/api/idiom')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        console.log(data);
-        console.log(data["Idiom"]);
 
         document.getElementById('Idiom').innerHTML = data["Idiom"];
         document.getElementById('Description').innerHTML = data['Meaning'];
@@ -37,12 +14,6 @@ function initialIdiom()
         
     });
    
-  
-   
-
-    
-   
-    
 }
 
 initialIdiom();
